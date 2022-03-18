89a6b9967805d8f2085b14594a5b1182764db114
samcho02 <sc107@illinois.edu>
2022-03-18 00:56:57 -0500

    created a detection border & text

--- scanner.py
+++ scanner.py
@@ -11,20 +11,30 @@ capture.set(4, 480)
 
 while True:
     success, img = capture.read()
-    cv.imshow('Result', img)
 
     for barcode in decode(img):
-            myData = barcode.data.decode('utf-8')       # coverts to barcode data to string
-            print(myData)
+        myData = barcode.data.decode('utf-8')       # coverts to barcode data to string
 
-            points = np.array([barcode.polygon], np.int32)
-            points = points.reshape[-1,1,2]
-            
-            cv.polylines(img, [points], True, (255,0,255), 5)   # polylines(filename, pts, IsClosed, Color, Thickness)
-    
-    
-    c = cv.waitKey(1)           
-    if c == 27:               # if the key pressed is esc, break loop
+        # Creates box over the detected code
+        points = np.array([barcode.polygon], np.int32)
+        points = points.reshape((-1,1,2))
+        cv.polylines(img, [points], True, (255,0,255), 5)   # polylines(filename, pts, IsClosed, Color, Thickness)
+        
+        # Puts text on top of the detected code
+        points2 = barcode.rect
+        cv.putText(img, myData, (points2[0], points2[1]), cv.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 255), 2)
+
+        # prints the data of the code
+        print(myData)
+
+    # displays img in the Scanner window
+    cv.imshow('Scanner', img)
+
+    # waits for key to pressed for 1 ms
+    c = cv.waitKey(1)       
+
+    # if the key pressed is esc, break loop
+    if c == 27:              
         break
 
 
