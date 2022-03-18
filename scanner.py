0436c5fe177cb923f476cf46aba7a64e01838196
samcho02 <sc107@illinois.edu>
2022-03-18 00:37:11 -0500

    some minor changes

--- scanner.py
+++ scanner.py
@@ -16,6 +16,12 @@ while True:
     for barcode in decode(img):
             myData = barcode.data.decode('utf-8')       # coverts to barcode data to string
             print(myData)
+
+            points = np.array([barcode.polygon], np.int32)
+            points = points.reshape[-1,1,2]
+            
+            cv.polylines(img, [points], True, (255,0,255), 5)   # polylines(filename, pts, IsClosed, Color, Thickness)
+    
     
     c = cv.waitKey(1)           
     if c == 27:               # if the key pressed is esc, break loop
