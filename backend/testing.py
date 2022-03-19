357d8a477e67d777c6c69c8e15267638bdfdc0a4
samcho02 <sc107@illinois.edu>
2022-03-19 16:45:27 -0500

    test completed

--- backend/testing.py
+++ backend/testing.py
@@ -1,1 +1,37 @@
 ## Testing Ground
+import cv2 as cv
+import numpy as np
+from pyzbar.pyzbar import decode
+
+myData = []
+
+
+def test():
+    cap = cv.VideoCapture(0, cv.CAP_DSHOW)
+    cap.set(3,640)
+    cap.set(4,480)
+
+    while True:
+        ret, img = cap.read()
+
+        if len(decode(img)) != 0:
+            for barcode in decode(img):
+                myData.append(barcode.data.decode('utf-8'))
+            global my
+            my = myData[0]
+            
+            print(myData)
+            print(my)
+            return False
+        
+        else:
+            cv.imshow('test',img)
+
+        c = cv.waitKey(1)
+        if c == 27:
+            break
+        
+    cap.release()
+    cv.destroyAllWindows()
+    
+test()
