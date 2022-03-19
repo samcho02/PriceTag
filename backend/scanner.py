22eb2a5b8a1dfc716f1f88fafce1bdf3ff5645c9
samcho02 <sc107@illinois.edu>
2022-03-19 16:46:15 -0500

    completed the barcode scanner beta version

--- backend/scanner.py
+++ backend/scanner.py
@@ -1,43 +1,39 @@
+## Testing Ground
 import cv2 as cv
 import numpy as np
 from pyzbar.pyzbar import decode
 
-## video capture 
-capture = cv.VideoCapture(0)
-# sets up the video window
-capture.set(3, 640)    
-capture.set(4, 480)    # height
 myData = []
 
-## Scans barcode & stores the barcode to myData
-def scan_barcode():
-    while capture.isOpened():
-        success, img = capture.read()
 
-        for barcode in decode(img):
-            if len(myData) == 0:
-                myData.append(barcode.data.decode('utf-8'))      # coverts to barcode data to string
-            # prints the data of the code
-                print(myData)
+def scanner():
+    cap = cv.VideoCapture(0, cv.CAP_DSHOW)
+    cap.set(3,640)
+    cap.set(4,480)
 
-            else:
-                break
+    while True:
+        ret, img = cap.read()
+
+        if len(decode(img)) != 0:
+            for barcode in decode(img):
+                myData.append(barcode.data.decode('utf-8'))
+
+            global my
+            my = myData[0]
+            
+            print(myData)
+            print(my)
+            return False
         
+        else:
+            cv.imshow('test',img)
 
-        # displays img in the Scanner window
-        cv.imshow('Scanner', img)
-
-        # waits for key to pressed for 1 ms
-        c = cv.waitKey(1)       
-
-        # if the key pressed is esc, break loop
-        if c == 27:              
+        # can always break out by pressing 'esc' key
+        c = cv.waitKey(1)
+        if c == 27:
             break
-
-
-## closes the vid cam
-    capture.release()
+        
+    cap.release()
     cv.destroyAllWindows()
-
-## Call
-scan_barcode()
+    
+scanner()
